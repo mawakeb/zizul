fe35120c2354022829f66427bd48e2505bb64549
cherin kim <76779953+mawakeb@users.noreply.github.com>
2026-02-24 00:03:18 +0900

    feat: add util file for dates

---
+++ zizul/lib/utils/date_range_util.dart
@@ -0,0 +1,31 @@
+class DateRange {
+  final int start;
+  final int end;
+
+  DateRange(this.start, this.end);
+}
+
+class DateRangeUtil {
+  static DateRange monthRange(DateTime date) {
+    final start = DateTime(date.year, date.month, 1);
+    final end = DateTime(date.year, date.month + 1, 1)
+        .subtract(const Duration(milliseconds: 1));
+    return DateRange(
+      start.millisecondsSinceEpoch,
+      end.millisecondsSinceEpoch,
+    );
+  }
+
+  static DateRange weekRange(DateTime date) {
+    final start = date.subtract(Duration(days: date.weekday - 1));
+    final weekStart =
+        DateTime(start.year, start.month, start.day);
+    final weekEnd =
+        weekStart.add(const Duration(days: 7))
+            .subtract(const Duration(milliseconds: 1));
+    return DateRange(
+      weekStart.millisecondsSinceEpoch,
+      weekEnd.millisecondsSinceEpoch,
+    );
+  }
+}
